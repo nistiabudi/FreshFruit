6be11cd4fd56fe5187e8cca4239855643c57d564
nistiabudi <92087512+nistiabudi@users.noreply.github.com>
2021-10-28 12:33:35 +0800

    Add files via upload

---
+++ FreshFruit/FreshFruit/model/Fruit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreshFruit.model
+{
+    class Fruit
+    {
+        public string name { get; set; }
+        public Fruit(string name)
+        {
+            this.name = name;
+        }
+        public string getName()
+        {
+            return this.name;
+        }
+    }
+}
